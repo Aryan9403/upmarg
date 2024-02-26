0ad79c74125bb6f852872b33ccd822b7ecdfcdb6
Aryan Kulkarni <kulkarnisaryan@gmail.com>
2024-02-26 11:51:55 -0600

    Add existing project files to Git

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // Make sure this imports before other components
 import App from './App';
-import 'path_to_your_pico_css_file.css'; // Path to PicoCSS file
 
 ReactDOM.render(
   <React.StrictMode>
