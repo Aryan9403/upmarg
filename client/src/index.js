3e7d56b3f823506f3e344f3083f3e3b22a2498c2
Aryan Kulkarni <kulkarnisaryan@gmail.com>
2024-02-26 12:32:02 -0600

    code done

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // Make sure this imports before other components
 import App from './App';
+import 'C:\Users\Aryan\Desktop\upmarg\client\src\Assets\Styles\pico-main';
+
 
 ReactDOM.render(
   <React.StrictMode>
