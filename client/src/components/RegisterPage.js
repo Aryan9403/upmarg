b9c2d9c5dd0a1070aecb8aa8dff9972a270d9644
Aryan Kulkarni <kulkarnisaryan@gmail.com>
2024-02-26 09:21:57 -0600

    Code Changed

--- client/src/components/RegisterPage.js
+++ client/src/components/RegisterPage.js
@@ -24,32 +24,30 @@ function RegisterPage() {
     }
   };
 
-  // The form and its elements should be returned from the component
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Register</button>
-    </form>
-  );
+  <form onSubmit={handleSubmit}>
+  <div>
+    <label htmlFor="email">Email:</label>
+    <input
+      type="email"
+      id="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      required
+    />
+  </div>
+  <div>
+    <label htmlFor="password">Password:</label>
+    <input
+      type="password"
+      id="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      required
+    />
+  </div>
+  <button type="submit">Register</button>
+</form>
+
 }
 
 export default RegisterPage;
